8a70737035f0dd313396cde5b0eb2b329db208b7
mawei <mawei@10.100.1.76>
2017-09-14 22:24:13 +0800

    增加练习初始化

---
+++ TextDemo/RNProject/demo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+  return {
+
+  };
+}
+
+export class demo extends React.Component {
+  static propTypes = {
+    name: React.PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps,
+// Implement map dispatch to props
+)(demo)
